86bc15c040ef74d25a8a4ac32738e6789765fb46
Jianxi Gao <j.gao@neu.edu>
2018-02-26 11:41:03 -0500

    Update Figure1_main.m

--- figure1/Figure1_main.m
+++ figure1/Figure1_main.m
@@ -1,5 +1,12 @@
 function Figure1_main
-
+%   Figure1_main Plots all the Figures 1 a - f in our paper "Universal
+%   resilience patterns in complex networks".
+%
+%  
+%   NuRsEv2.0
+%   $Id: Figure1_main.m Created at 2018-2-26 11:35:29 $
+%   by Jianxi Gao, RPI, Troy, NY
+%   Copyright (c) 2012-2018 by Center for Complex Network Research (CCNR)
 
 %% plot figure 1 a
 Figure1_a
